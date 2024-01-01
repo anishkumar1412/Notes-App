4a334246ea7b0b1bd6e1677a1e4060df684d41a8
anishkumar1412 <106010120+anishkumar1412@users.noreply.github.com>
2024-01-02 02:41:09 +0530

    Update script.js fixing some known bugs

--- script.js
+++ script.js
@@ -54,7 +54,7 @@ icon.onclick=function(){
 
     document.body.classList.toggle("dark-theme");
     if(document.body.classList.contains("dark-theme")){
-       icon.innerHTML=' <i class="fa-solid fa-moon "></i>'
+       icon.innerHTML=' <i class="fa-solid fa-moon  "></i>'
 
     }
     else{
